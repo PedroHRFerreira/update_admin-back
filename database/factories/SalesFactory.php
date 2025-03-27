d0a5654e345a6d8206d001f3e3ccb843473d0fda
PedroHRFerreira <devpedrohr@gmail.com>
2025-03-27 16:14:01 -0300

    feat: :sparkles: Ajuustes necessarios

--- database/factories/SalesFactory.php
+++ database/factories/SalesFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sales>
@@ -17,6 +18,7 @@ class SalesFactory extends Factory
     public function definition(): array
     {
         return [
+            'id_user' => User::inRandomOrder()->value('id'), 
             'name' => fake()->name(),
             'description' => fake()->text(),
         ];
--- database/migrations/2025_03_27_172202_create_sales_table.php
+++ database/migrations/2025_03_27_172202_create_sales_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+
             $table->string('name');
             $table->string('description')->nullable();
             $table->timestamps();
