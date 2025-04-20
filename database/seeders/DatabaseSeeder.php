0545ce4c383003c5286c460764fd4c569c95cace
PedroHRFerreira <devpedrohr@gmail.com>
2025-04-20 15:20:00 -0300

    fix: :bug: Ajustes para produção

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            UsersSeeder::class,
-            ProductsSeeder::class, 
-            SalesSeeder::class,
-            ExpensesSeeder::class,
+            // UsersSeeder::class,
+            // ProductsSeeder::class, 
+            // SalesSeeder::class,
+            // ExpensesSeeder::class,
             GoalsSeeder::class
         ]);
     }
