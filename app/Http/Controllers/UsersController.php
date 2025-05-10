3f77d4507f041fe1cfa0441c3fcb2d481e96ade4
PedroHRFerreira <devpedrohr@gmail.com>
2025-05-09 23:32:11 -0300

    feat: :sparkles: rotas de users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,87 @@ class UsersController extends Controller
             'code' => 200
         ]);
     }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => 'nullable|string',
+        ]);
+
+        $users = User::create($validated);
+
+        if (!$users) {
+            return response()->json([
+                'user' => [],
+                'message' => 'Erro ao criar usuario',
+                'status' => 'error',
+                'code' => 422
+            ], 422);
+        }
+
+        return response()->json([
+            'user' => $users,
+            'message' => 'Usuario criado com sucesso',
+            'status' => 'success',
+            'code' => 200
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+    $user = User::find($id);
+
+    if (!$user) {
+        return response()->json([
+            'user' => [],
+            'message' => 'Usuário não encontrado',
+            'status' => 'error',
+            'code' => 404
+        ], 404);
+    }
+
+    $validated = $request->validate([
+        'name' => 'sometimes|required|string|max:255',
+        'email' => 'sometimes|required|string|lowercase|email|max:255|unique:users,email,' . $user->id,
+        'password' => 'nullable|string',
+    ]);
+
+    if (!empty($validated['password'])) {
+        $validated['password'] = bcrypt($validated['password']);
+    } else {
+        unset($validated['password']);
+    }
+
+    $user->update($validated);
+
+    return response()->json([
+        'user' => $user,
+        'message' => 'Usuário atualizado com sucesso',
+        'status' => 'success',
+        'code' => 200
+    ]);
+    }
+
+
+    public function destroy($id)
+    {
+        $users = User::find($id);
+
+        if (!$users) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Usuário não encontrada'
+            ], 404);
+        }
+
+        $users->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Usuário removida com sucesso'
+        ], 200);
+    }
 }
